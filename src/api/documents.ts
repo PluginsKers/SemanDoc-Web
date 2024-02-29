bd3576e6c45c7fa0a5ab8d7c2fc17d3a48e2782d
PluginsKers <ikers@foxmail.comm>
2024-03-01 00:38:48 +0800

    fix: dev with no /login router

--- src/api/documents.ts
+++ src/api/documents.ts
@@ -1,15 +1,46 @@
 import axios from 'axios';
 import { Document } from './types';
+import router from '../router';
 
 const API_BASE_URL = 'https://ai.app.nbpt.edu.cn/api';
+const http = axios.create({
+    baseURL: API_BASE_URL,
+    timeout: 6000
+});
+
+http.interceptors.request.use(
+    function (config) {
+        const token = localStorage.getItem('token');
+        config.headers.Authorization = `Bearer ${token}`;
+        return config
+    }
+)
+
+http.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        if (error.response) {
+            const status = error.response.status;
+            if (status === 401 || status === 403) {
+                localStorage.removeItem('token');
+                router.push("/login");
+                return Promise.reject("验证已过期");
+            }
+        }
+        return Promise.reject(error);
+    }
+);
+
 
 export const queryDocuments = async (query: string, k: number, filter: object): Promise<Document[]> => {
-    const response = await axios.post(`${API_BASE_URL}/query`, { query, k, filter });
+    const response = await http.post(`/query`, { query, k, filter });
     return response.data.data;
 };
 
 export const removeDocuments = async (target: number[]) => {
-    const response = await axios.post(`${API_BASE_URL}/edit/remove`, {
+    const response = await http.post(`/edit/remove`, {
         type: "ids",
         target
     });
@@ -17,7 +48,7 @@ export const removeDocuments = async (target: number[]) => {
 };
 
 export const addDocument = async (data: Document["page_content"], metadata: Document["metadata"]): Promise<Document[]> => {
-    const response = await axios.post(`${API_BASE_URL}/edit/add`, {
+    const response = await http.post(`/edit/add`, {
         data,
         metadata
     });
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,7 @@
 import { createApp } from 'vue';
-import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 
 import './style.css'
-
-axios.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-}, error => {
-    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-        localStorage.removeItem('token');
-        router.push('/login');
-        return Promise.reject("验证已过期");
-    }
-    return Promise.reject(error);
-});
 
 createApp(App).use(router).mount('#app');
